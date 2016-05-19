1758d18a27970f84f72a4b9d9f21e0c9bdeee239
Samir <samirshah@deloitte.com>
2016-05-19 16:38:47 +0530

    adding ui router

--- source/modules/app.js
+++ source/modules/app.js
@@ -1,11 +1,21 @@
 'use strict';
-var app = angular.module('nuSkinApp', ['ngRoute'])
+var app = angular.module('nuSkinApp', ['ui.router'])
 //Declare all your project constants here
 .constant('CUSTOMER_CARE_NUMBER', '1-999-999-9999')
 //Declare all your project configurations here
-.config(['$routeProvider', function ($routeProvider) {
+app.config(function($stateProvider, $urlRouterProvider){
+$stateProvider
+        .state('login', {
+            url: '/login',
+            templateUrl: 'modules/accounts/login/login.html'
+        })
+        .state('signup', {
+            url: '/signup',
+            templateUrl: 'modules/accounts/signup/signup.html'
+        });
 
-}]);
+    $urlRouterProvider.otherwise('/login');
+});
 
 //Run method
 app.run(function(){
