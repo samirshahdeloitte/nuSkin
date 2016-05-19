e6e12cb4e6e5ef352f70fa672dc26b8a86106a6a
Luke Issac <lissac@deloitte.com>
2016-05-19 15:42:53 +0530

    nitial commit

--- src/js/factories/memorycache.js
+++
@@ -1,9 +0,0 @@
-(function() {
-	'use strict';
-
-	angular
-		.module('nuSkinApp')
-		.factory('MemoryCache', function($cacheFactory) {
-			return $cacheFactory('memoryCache');
-		})
-});
--- src/js/services/shared-data.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-app.service('SharedDataSvc', ['$cookieStore', function ($cookieStore) {
-
-}]);
